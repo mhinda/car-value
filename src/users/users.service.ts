5285d2cc3deff5fcc86570bce13f5c953527bad2
Amani Mhinda <amani.mhinda@gmail.com>
2024-03-07 18:15:06 +0300

    Implementing user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User) private repo: Repository<User>){}
+
+    create(email: string, password: string){
+        const user = this.repo.create({email, password});
+        
+        return this.repo.save(user);
+    }
+
+    findOneBy(id: number) {
+        return this.repo.findOneBy({id});
+    }
+
+    findAll() {
+        return this.repo.find();
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.findOneBy(id);
+        if (!user) throw new Error('User not found')
+    
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.findOneBy(id);
+        if (!user) throw new Error('User not found')
+    
+        return this.repo.remove(user);
+    }
+}
